63e4235345a6a4bc6bd7fec8759d8ba9e4830c12
VisPatrick <pksantos1003@gmail.com>
2025-05-13 20:56:35 -0300

    atividade1 - Calculadora

---
+++ aula10/atividade_01.py
@@ -0,0 +1,42 @@
+# criar uma calculadora que soma, subtrai, multiplica e divide dois números inteiros
+# criar uma função para cada operação
+# biblioteca para números aleatorios
+# função soma
+import os
+os.system('cls')
+
+
+
+def soma(n1, n2):
+    return n1 + n2
+
+
+# função subtração
+def subtracao(n1, n2):
+    return n1 - n2
+
+
+# função multiplicação
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+
+# função divisão
+def divisao(n1, n2):
+    return n1/n2
+
+num1 = float(input('\nDigite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+print(30*"=")
+
+somar = soma(num1, num2)
+print(f"Soma: {somar}")
+
+sub = subtracao(num1, num2)
+print(f"Subtração: {sub}")
+
+mult = multiplicacao(num1, num2)
+print(f"Multiflicação: {mult}")
+
+div = divisao(num1, num2)
+print(f"Divisão: {div}")
